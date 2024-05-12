8b0a5369cf443b0c4c797117aeb24c99d8509f58
myapos <myapos@gmail.com>
2024-05-12 12:25:17 +0300

    added more game details and instructions

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -11,7 +11,28 @@
       </div>
       <transition name="game-details">
         <div class="gameDetails" v-show="showDetails" data-testid="details">
-          Looking for {{ M }} consecutive values in same row, column, primary or secondary diagonal
+          <p>
+            Note: This is an advanced version of Tic-tac-toe. This assignment was published in
+            <a href="https://www.greatfrontend.com/" target="_blank"> Greatfrontend </a>platform.
+          </p>
+          <p>
+            Tic-tac-toe is a game for two players who take turns making space in a three-by-three
+            grid with X or O. The player who succeeds in playing three of their marks in a
+            horizontal, vertical, or diagonal row is the winner. Source:
+            <a href="https://en.wikipedia.org/wiki/Tic-tac-toe" target="_blank">Wikipedia</a>
+          </p>
+          <p>
+            Traditionally, tic-tac-toe uses a 3 x 3 grid. In this problem, an advanced version of a
+            tic-tac-toe game is built where the grid has N x N cells on the board and needs M marks
+            in a horizontal, vertical, or diagonal row to win. The following diagram shows an
+            example game where N = {{ N }} and M = {{ M }}.
+          </p>
+          <p>
+            You can change these parameters by adding to the url parameters
+            <span class="highlight-text">?N=4&M=3</span> where
+            <span class="highlight-text">N >= M</span>.
+            <a href="/?N=4&M=4">Try it!</a>
+          </p>
         </div>
       </transition>
     </app-controls>
@@ -216,6 +237,12 @@ export default defineComponent({
   color: var(--vt-c-text-dark-2);
   padding: 5px;
   text-align: center;
+  max-width: 500px;
+  text-align: left;
+}
+
+.gameDetails p {
+  padding: 5px;
 }
 
 .row {
