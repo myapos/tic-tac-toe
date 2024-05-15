197d395eba7720f83eaf5c08e1d68bd04358a14d
myapos <myapos@gmail.com>
2024-05-15 23:42:27 +0300

    added alpha beta prunning and fine tuned maximum depth for large grids

--- src/components/utils/minimax.ts
+++ src/components/utils/minimax.ts
@@ -1,10 +1,11 @@
 import type { Ref } from 'vue'
 import type { gridT } from '@/components/types'
-import { O, X, MAX_DEPTH } from '@/constants'
+import { O, X } from '@/constants'
 import isDraw from './checkUtils/isDraw'
 import checkAiWinner from './checkAiWinner'
 import findEmptyCells from './findEmptyCells'
 import { getBestScoreAndMove } from './findBestMove'
+import { getMaxDepth } from './getMaxDepth'
 
 interface minimaxI {
   grid: gridT
@@ -14,6 +15,8 @@ interface minimaxI {
   isMaximizing: boolean
   counter: Ref<number>
   setCounter: (value: number) => void
+  alpha: number
+  beta: number
 }
 export const minimax = ({
   grid,
@@ -22,7 +25,9 @@ export const minimax = ({
   counter,
   setCounter,
   M,
-  isXTurn
+  isXTurn,
+  alpha,
+  beta
 }: minimaxI) => {
   const result = checkAiWinner({ grid, shouldMarkWinningCells: false, M, isXTurn })
   // Check draw as last step if no one wins
@@ -30,12 +35,7 @@ export const minimax = ({
     return 0
   }
 
-  if (result.won) {
-    const emptyCells = findEmptyCells(grid)
-    return isMaximizing ? 1 * (emptyCells + 1) : -1 * (emptyCells + 1)
-  }
-
-  if (depth === MAX_DEPTH) {
+  if (result.won || depth >= getMaxDepth({ grid })) {
     const emptyCells = findEmptyCells(grid)
     return isMaximizing ? 1 * (emptyCells + 1) : -1 * (emptyCells + 1)
   }
@@ -49,7 +49,9 @@ export const minimax = ({
       counter,
       M,
       player: X,
-      isMaximizing
+      isMaximizing,
+      alpha,
+      beta
     })
     return bestScore
   }
@@ -62,7 +64,9 @@ export const minimax = ({
     counter,
     M,
     player: O,
-    isMaximizing
+    isMaximizing,
+    alpha,
+    beta
   })
   return bestScore
 }
